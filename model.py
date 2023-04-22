8d5675dbc9c5a3046ed837113f9ad9b5fd452e9d
mk-eller <63464794+mk-eller@users.noreply.github.com>
2023-04-22 13:19:41 -0400

    Update Model

--- model.py
+++ model.py
@@ -3,18 +3,13 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import nltk
 nltk.download('stopwords')
-from nltk.corpus import stopwords
-import re
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
 import pickle
+from sklearn.tree import DecisionTreeClassifier
 
 fileName1 = "ModelData.csv"
 data = pd.read_csv(fileName1)
@@ -27,15 +22,16 @@ x = data.ingredients
 y = data.tags
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state = 42)
 
-from sklearn.ensemble import RandomForestClassifier
-rfc = Pipeline([('vect', CountVectorizer()),
+dtc = Pipeline([('vect', CountVectorizer()),
  ('tfidf', TfidfTransformer()),
- ('clf', RandomForestClassifier())
+ ('clf', DecisionTreeClassifier())
  ])
-rfc.fit(x_train, y_train)
-y_pred = rfc.predict(x_test)
+dtc.set_params(clf__random_state=42,
+              clf__max_features=600)
+dtc.fit(x_train, y_train)
+y_pred4 = dtc.predict(x_test)
 
-pickle.dump(rfc, open('model.pkl','wb'))
+pickle.dump(dtc, open('model.pkl','wb'))
 
 
 
